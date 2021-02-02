ed1f1966288bc507fa6ce457d81bf99cdbc12c56
SuyogiMehta <68965718+SuyogiMehta@users.noreply.github.com>
2021-02-02 23:29:45 +0530

    Add files via upload

---
+++ C Housie Game-script.js
@@ -0,0 +1,339 @@
+function my() {
+    // var a = document.getElementById("b").nodeValue;
+    // var b = document.getElementById("d").nodeValue;
+    // var c = document.getElementById("f").nodeValue;
+    // var d = document.getElementById("h").nodeValue;
+
+    // var e = document.getElementById("k").nodeValue;
+    // var f = document.getElementById("m").nodeValue;
+    // var g = document.getElementById("o").nodeValue;
+    // var h = document.getElementById("q").nodeValue;
+
+
+
+    // var i = document.getElementById("bA").nodeValue;
+    // var j = document.getElementById("dA").nodeValue;
+    // var k = document.getElementById("fA").nodeValue;
+    // var l = document.getElementById("hA").nodeValue;
+
+    // var m = document.getElementById("kA").nodeValue;
+    // var n = document.getElementById("mA").nodeValue;
+    // var o = document.getElementById("oA").nodeValue;
+    // var p = document.getElementById("qA").nodeValue;
+
+
+
+
+
+    document.getElementById("b").innerHTML = Math.floor((Math.random() * 6) + 1);
+    document.getElementById("d").innerHTML = Math.floor((Math.random() * 6) + 7);
+    document.getElementById("f").innerHTML = Math.floor((Math.random() * 6) + 13);
+    document.getElementById("h").innerHTML = Math.floor((Math.random() * 6) + 19);
+
+    document.getElementById("k").innerHTML = Math.floor((Math.random() * 6) + 25);
+    document.getElementById("m").innerHTML = Math.floor((Math.random() * 6) + 31);
+    document.getElementById("o").innerHTML = Math.floor((Math.random() * 6) + 37);
+    document.getElementById("q").innerHTML = Math.floor((Math.random() * 6) + 43);
+
+
+
+    document.getElementById("bA").innerHTML = Math.floor((Math.random() * 6) + 49);
+    document.getElementById("dA").innerHTML = Math.floor((Math.random() * 6) + 55);
+    document.getElementById("fA").innerHTML = Math.floor((Math.random() * 6) + 61);
+    document.getElementById("hA").innerHTML = Math.floor((Math.random() * 6) + 67);
+
+    document.getElementById("kA").innerHTML = Math.floor((Math.random() * 6) + 74);
+    document.getElementById("mA").innerHTML = Math.floor((Math.random() * 6) + 81);
+    document.getElementById("oA").innerHTML = Math.floor((Math.random() * 6) + 88);
+    document.getElementById("qA").innerHTML = Math.floor((Math.random() * 6) + 95);
+
+}
+
+
+
+
+
+function my1() {
+    if ((document.getElementById("b").innerHTML == document.getElementById("vijay").innerHTML) && document.getElementById("b").style.backgroundColor != "white") {
+    document.getElementById("b").style.background = "white";
+    document.getElementById("b").style.color = "transparent";
+    } else if (document.getElementById("b").style.backgroundColor != "white") {
+        alert("Sorry, but the recent shown number does not match with this number.");
+        alert("Please don't be a cheater next time!!");
+    }
+}
+
+function my2() {
+    if ((document.getElementById("d").innerHTML == document.getElementById("vijay").innerHTML) && document.getElementById("d").style.backgroundColor != "white") {
+        document.getElementById("d").style.background = "white";
+        document.getElementById("d").style.color = "transparent";
+        } else if (document.getElementById("d").style.backgroundColor != "white") {
+            alert("Sorry, but the recent shown number does not match with this number.");
+            alert("Please don't be a cheater next time!!");
+        }
+}
+
+function my3() {
+    if ((document.getElementById("f").innerHTML == document.getElementById("vijay").innerHTML) && document.getElementById("f").style.backgroundColor != "white") {
+        document.getElementById("f").style.background = "white";
+        document.getElementById("f").style.color = "transparent";
+        } else if (document.getElementById("f").style.backgroundColor != "white") {
+            alert("Sorry, but the recent shown number does not match with this number.");
+            alert("Please don't be a cheater next time!!");
+        }
+}
+
+function my4() {
+    if ((document.getElementById("h").innerHTML == document.getElementById("vijay").innerHTML) && document.getElementById("h").style.backgroundColor != "white") {
+        document.getElementById("h").style.background = "white";
+        document.getElementById("h").style.color = "transparent";
+        } else if (document.getElementById("h").style.backgroundColor != "white") {
+            alert("Sorry, but the recent shown number does not match with this number.");
+            alert("Please don't be a cheater next time!!");
+        }
+}
+
+function my5() {
+    if ((document.getElementById("k").innerHTML == document.getElementById("vijay").innerHTML) && document.getElementById("k").style.backgroundColor != "white") {
+        document.getElementById("k").style.background = "white";
+        document.getElementById("k").style.color = "transparent";
+        } else if (document.getElementById("k").style.backgroundColor != "white") {
+            alert("Sorry, but the recent shown number does not match with this number.");
+            alert("Please don't be a cheater next time!!");
+        }
+}
+
+function my6() {
+    if ((document.getElementById("m").innerHTML == document.getElementById("vijay").innerHTML) && document.getElementById("m").style.backgroundColor != "white") {
+        document.getElementById("m").style.background = "white";
+        document.getElementById("m").style.color = "transparent";
+        } else if (document.getElementById("m").style.backgroundColor != "white") {
+            alert("Sorry, but the recent shown number does not match with this number.");
+            alert("Please don't be a cheater next time!!");
+        }
+}
+
+function my7() {
+    if ((document.getElementById("o").innerHTML == document.getElementById("vijay").innerHTML) && document.getElementById("o").style.backgroundColor != "white") {
+        document.getElementById("o").style.background = "white";
+        document.getElementById("o").style.color = "transparent";
+        } else if (document.getElementById("o").style.backgroundColor != "white") {
+            alert("Sorry, but the recent shown number does not match with this number.");
+            alert("Please don't be a cheater next time!!");
+        }
+}
+
+function my8() {
+    if ((document.getElementById("q").innerHTML == document.getElementById("vijay").innerHTML) && document.getElementById("q").style.backgroundColor != "white") {
+        document.getElementById("q").style.background = "white";
+        document.getElementById("q").style.color = "transparent";
+        } else if (document.getElementById("q").style.backgroundColor != "white") {
+            alert("Sorry, but the recent shown number does not match with this number.");
+            alert("Please don't be a cheater next time!!");
+        }
+}
+
+
+
+
+
+
+function my9() {
+    if ((document.getElementById("bA").innerHTML == document.getElementById("vijay").innerHTML) && document.getElementById("bA").style.backgroundColor != "white") {
+        document.getElementById("bA").style.background = "white";
+        document.getElementById("bA").style.color = "transparent";
+        } else if (document.getElementById("bA").style.backgroundColor != "white") {
+            alert("Sorry, but the recent shown number does not match with this number.");
+            alert("Please don't be a cheater next time!!");
+        }
+}
+
+function my10() {
+    if ((document.getElementById("dA").innerHTML == document.getElementById("vijay").innerHTML) && document.getElementById("dA").style.backgroundColor != "white") {
+        document.getElementById("dA").style.background = "white";
+        document.getElementById("dA").style.color = "transparent";
+        } else if (document.getElementById("dA").style.backgroundColor != "white") {
+            alert("Sorry, but the recent shown number does not match with this number.");
+            alert("Please don't be a cheater next time!!");
+        }
+}
+
+function my11() {
+    if ((document.getElementById("fA").innerHTML == document.getElementById("vijay").innerHTML) && document.getElementById("fA").style.backgroundColor != "white") {
+        document.getElementById("fA").style.background = "white";
+        document.getElementById("fA").style.color = "transparent";
+        } else if (document.getElementById("fA").style.backgroundColor != "white") {
+            alert("Sorry, but the recent shown number does not match with this number.");
+            alert("Please don't be a cheater next time!!");
+        }
+}
+
+function my12() {
+    if ((document.getElementById("hA").innerHTML == document.getElementById("vijay").innerHTML) && document.getElementById("hA").style.backgroundColor != "white") {
+        document.getElementById("hA").style.background = "white";
+        document.getElementById("hA").style.color = "transparent";
+        } else if (document.getElementById("hA").style.backgroundColor != "white") {
+            alert("Sorry, but the recent shown number does not match with this number.");
+            alert("Please don't be a cheater next time!!");
+        }
+}
+
+function my13() {
+    if ((document.getElementById("kA").innerHTML == document.getElementById("vijay").innerHTML) && document.getElementById("kA").style.backgroundColor != "white") {
+        document.getElementById("kA").style.background = "white";
+        document.getElementById("kA").style.color = "transparent";
+        } else if (document.getElementById("kA").style.backgroundColor != "white") {
+            alert("Sorry, but the recent shown number does not match with this number.");
+            alert("Please don't be a cheater next time!!");
+        }
+}
+
+function my14() {
+    if ((document.getElementById("mA").innerHTML == document.getElementById("vijay").innerHTML) && document.getElementById("mA").style.backgroundColor != "white") {
+        document.getElementById("mA").style.background = "white";
+        document.getElementById("mA").style.color = "transparent";
+        } else if (document.getElementById("mA").style.backgroundColor != "white") {
+            alert("Sorry, but the recent shown number does not match with this number.");
+            alert("Please don't be a cheater next time!!");
+        }
+}
+
+function my15() {
+    if ((document.getElementById("oA").innerHTML == document.getElementById("vijay").innerHTML) && document.getElementById("oA").style.backgroundColor != "white") {
+        document.getElementById("oA").style.background = "white";
+        document.getElementById("oA").style.color = "transparent";
+        } else if (document.getElementById("oA").style.backgroundColor != "white") {
+            alert("Sorry, but the recent shown number does not match with this number.");
+            alert("Please don't be a cheater next time!!");
+        }
+}
+
+function my16() {
+    if ((document.getElementById("qA").innerHTML == document.getElementById("vijay").innerHTML) && document.getElementById("qA").style.backgroundColor != "white") {
+        document.getElementById("qA").style.background = "white";
+        document.getElementById("qA").style.color = "transparent";
+        } else if (document.getElementById("qA").style.backgroundColor != "white") {
+            alert("Sorry, but the recent shown number does not match with this number.");
+            alert("Please don't be a cheater next time!!");
+        }
+}
+
+function start() {
+    document.getElementById("mehta").innerHTML = Math.floor((Math.random() * 100) + 1);
+    // if (document.getElementById("mehta").innerHTML == null) {
+    //     start();
+    //     // alert("success");
+    // }
+    if (!document.getElementById(document.getElementById("mehta").innerHTML)) {
+        if (!document.getElementById("1") &&
+            !document.getElementById("2") &&
+            !document.getElementById("3") &&
+            !document.getElementById("4") &&
+            !document.getElementById("5") &&
+            !document.getElementById("6") &&
+            !document.getElementById("7") &&
+            !document.getElementById("8") &&
+            !document.getElementById("9") &&
+            !document.getElementById("10") &&
+            !document.getElementById("11") &&
+            !document.getElementById("12") &&
+            !document.getElementById("13") &&
+            !document.getElementById("14") &&
+            !document.getElementById("15") &&
+            !document.getElementById("16") &&
+            !document.getElementById("17") &&
+            !document.getElementById("18") &&
+            !document.getElementById("19") &&
+            !document.getElementById("20") &&
+            !document.getElementById("21") &&
+            !document.getElementById("22") &&
+            !document.getElementById("23") &&
+            !document.getElementById("24") &&
+            !document.getElementById("25") &&
+            !document.getElementById("26") &&
+            !document.getElementById("27") &&
+            !document.getElementById("28") &&
+            !document.getElementById("29") &&
+            !document.getElementById("30") &&
+            !document.getElementById("31") &&
+            !document.getElementById("32") &&
+            !document.getElementById("33") &&
+            !document.getElementById("34") &&
+            !document.getElementById("35") &&
+            !document.getElementById("36") &&
+            !document.getElementById("37") &&
+            !document.getElementById("38") &&
+            !document.getElementById("39") &&
+            !document.getElementById("40") &&
+            !document.getElementById("41") &&
+            !document.getElementById("42") &&
+            !document.getElementById("43") &&
+            !document.getElementById("44") &&
+            !document.getElementById("45") &&
+            !document.getElementById("46") &&
+            !document.getElementById("47") &&
+            !document.getElementById("48") &&
+            !document.getElementById("49") &&
+            !document.getElementById("50") &&
+            !document.getElementById("51") &&
+            !document.getElementById("52") &&
+            !document.getElementById("53") &&
+            !document.getElementById("54") &&
+            !document.getElementById("55") &&
+            !document.getElementById("56") &&
+            !document.getElementById("57") &&
+            !document.getElementById("58") &&
+            !document.getElementById("59") &&
+            !document.getElementById("60") &&
+            !document.getElementById("61") &&
+            !document.getElementById("62") &&
+            !document.getElementById("63") &&
+            !document.getElementById("64") &&
+            !document.getElementById("65") &&
+            !document.getElementById("66") &&
+            !document.getElementById("67") &&
+            !document.getElementById("68") &&
+            !document.getElementById("69") &&
+            !document.getElementById("70") &&
+            !document.getElementById("71") &&
+            !document.getElementById("72") &&
+            !document.getElementById("73") &&
+            !document.getElementById("74") &&
+            !document.getElementById("75") &&
+            !document.getElementById("76") &&
+            !document.getElementById("77") &&
+            !document.getElementById("78") &&
+            !document.getElementById("79") &&
+            !document.getElementById("80") &&
+            !document.getElementById("81") &&
+            !document.getElementById("82") &&
+            !document.getElementById("83") &&
+            !document.getElementById("84") &&
+            !document.getElementById("85") &&
+            !document.getElementById("86") &&
+            !document.getElementById("87") &&
+            !document.getElementById("88") &&
+            !document.getElementById("89") &&
+            !document.getElementById("90") &&
+            !document.getElementById("91") &&
+            !document.getElementById("92") &&
+            !document.getElementById("93") &&
+            !document.getElementById("94") &&
+            !document.getElementById("95") &&
+            !document.getElementById("96") &&
+            !document.getElementById("97") &&
+            !document.getElementById("98") &&
+            !document.getElementById("99") &&
+            !document.getElementById("100") ) {
+            document.getElementById("vijay").innerHTML = "Housie Game is Over!";
+        } else {
+        start();
+        }
+    }
+    if (document.getElementById(document.getElementById("mehta").innerHTML)) {
+    var numdecided = 
+    document.getElementById(document.getElementById("mehta").innerHTML).id;
+        document.getElementById(document.getElementById("mehta").innerHTML).remove();
+        document.getElementById("vijay").innerHTML = numdecided;
+    }
+}
